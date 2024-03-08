965319fdc1239e24a580cfd872c55d82af7821f5
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-08 15:53:08 +0300

    Create LAB-3-Control-flow-if-else.dart

---
+++ LAB-3-Control-flow-if-else.dart
@@ -0,0 +1,10 @@
+
+import 'dart:io';
+void main() {
+  stdout.writeln('Insert a number: ');
+  var number = int.parse(stdin.readLineSync()!);
+  if (number % 2 == 0) {
+    print('$number is even');
+  } else {}
+  print('$number is odd');
+}
