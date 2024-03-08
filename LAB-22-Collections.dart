095b82b418ba7e06133efd7d5b399891d49feab3
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-08 17:27:14 +0300

    Create LAB-22-Collections.dart

---
+++ LAB-22-Collections.dart
@@ -0,0 +1,22 @@
+void main() {
+  List<String> favoriteHobbies = []; 
+
+  print('My Favorite Hobbies:');
+  print('Initial list: ${favoriteHobbies.toString()}');
+
+
+
+  favoriteHobbies.add('Writing poem');
+  favoriteHobbies.add('Reading Self help books');
+  favoriteHobbies.add('Listening Mezmur');
+
+  print('Updated list : ${favoriteHobbies.toString()}');
+  favoriteHobbies.remove('Writing poem');
+
+  print('Updated list: ${favoriteHobbies.toString()}');
+
+
+  favoriteHobbies.sort();
+
+  print('Is the list empty? ${favoriteHobbies.isEmpty}');
+}
