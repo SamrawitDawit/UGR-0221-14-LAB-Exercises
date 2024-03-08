421f033ebf128f67e3b01f0abc718a43afb67204
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-08 16:02:06 +0300

    Update LAB-6-Control-flow-switch.dart

--- LAB-6-Control-flow-switch.dart
+++ LAB-6-Control-flow-switch.dart
@@ -1,1 +1,23 @@
+import 'dart:io';
 
+void main() {
+  stdout.writeln('Insert grade: ');
+  var grade = int.parse(stdin.readLineSync()!);
+  grade = grade ~/ 10;
+  switch (grade) {
+    case 9:
+      print('A');
+      break;
+    case 8:
+      print('B');
+      break;
+    case 7:
+      print('C');
+      break;
+    case 6:
+      print('D');
+      break;
+    default:
+      print('F');
+  }
+}
