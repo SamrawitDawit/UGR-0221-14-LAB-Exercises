eafd7cb800a6bf6a0a5ffa5549fd8542738524ce
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-08 14:58:09 +0300

    Update LAB-1-Variables-and-types.dart

--- LAB-1-Variables-and-types.dart
+++ LAB-1-Variables-and-types.dart
@@ -1,1 +1,7 @@
-
+main() {
+  String name = 'Samrawit Dawit';
+  int age = 20;
+  String favorite_color = 'White';
+  print(
+      'my name is $name. I am $age years old. My favorite color is $favorite_color');
+}
