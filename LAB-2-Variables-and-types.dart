acea08774bf7aa2f7d489132a843151786003931
SamrawitDawit <115358870+SamrawitDawit@users.noreply.github.com>
2024-03-08 15:23:22 +0300

    Update LAB-2-Variables-and-types.dart

--- LAB-2-Variables-and-types.dart
+++ LAB-2-Variables-and-types.dart
@@ -1,7 +1,7 @@
-main() {
+void main() {
   const int c = 299792458;
   stdout.writeln('Insert distance: ');
   var s = double.parse(stdin.readLineSync()!);
   double t = s / c;
-  print('time to travel in $s distance is $t');
+  print('time to travel in $s distance is $t m/s');
 }
